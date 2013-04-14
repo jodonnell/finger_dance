63b9448592dd702bdf5375e424df1fcf23e43d87
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-04-14 15:35:10 -0400

    changes

--- main_game.lua
+++ main_game.lua
@@ -16,11 +16,12 @@ function MainGame:init()
    self:loadSound()
    self:printNotes()
 
-   notes = Notes()
+   self.notes = Notes()
    self:playSong(1)
 end
 
 function MainGame:playSong(startTime)
+   -- from F1 to A4
    self:measure1(startTime)
    self:measure2(startTime + 8)
    self:measure3(startTime + 17)
@@ -37,158 +38,158 @@ function MainGame:playSong(startTime)
 end
 
 function MainGame:measure1(startTime)
-   notes:createNote("C3", 1, startTime)
-   notes:createNote("C3", 1, startTime + 2)
+   self.notes:createNote("C3", 1, startTime)
+   self.notes:createNote("C3", 1, startTime + 2)
 
-   notes:createNote("E3", 1, startTime + 3)
-   notes:createNote("E3", 1, startTime + 5)
+   self.notes:createNote("E3", 1, startTime + 3)
+   self.notes:createNote("E3", 1, startTime + 5)
 
-   notes:createNote("A2", 1, startTime + 6)
+   self.notes:createNote("A2", 1, startTime + 6)
 end
 
 function MainGame:measure2(startTime)
-   notes:createNote("A2", 1, startTime)
+   self.notes:createNote("A2", 1, startTime)
 
-   notes:createNote("C3", 1, startTime + 1)
-   notes:createNote("C3", 1, startTime + 3)
+   self.notes:createNote("C3", 1, startTime + 1)
+   self.notes:createNote("C3", 1, startTime + 3)
 
-   notes:createNote("D3", 1, startTime + 4)
-   notes:createNote("D3", 1, startTime + 6)
+   self.notes:createNote("D3", 1, startTime + 4)
+   self.notes:createNote("D3", 1, startTime + 6)
 
-   notes:createNote("F3", 1, startTime + 7)
+   self.notes:createNote("F3", 1, startTime + 7)
 end
 
 function MainGame:measure3(startTime)
-   notes:createNote("F3", 1, startTime)
+   self.notes:createNote("F3", 1, startTime)
 
-   notes:createNote("G2", 1, startTime + 1)
-   notes:createNote("G2", 1, startTime + 3)
+   self.notes:createNote("G2", 1, startTime + 1)
+   self.notes:createNote("G2", 1, startTime + 3)
 
-   notes:createNote("B2", 1, startTime + 4)
-   notes:createNote("B2", 1, startTime + 6)
+   self.notes:createNote("B2", 1, startTime + 4)
+   self.notes:createNote("B2", 1, startTime + 6)
 end
 
 function MainGame:measure4(startTime)
-   notes:createNote("C3", 1, startTime)
-   notes:createNote("C3", 1, startTime + 2)
+   self.notes:createNote("C3", 1, startTime)
+   self.notes:createNote("C3", 1, startTime + 2)
 
-   notes:createNote("E3", 1, startTime + 3)
-   notes:createNote("E3", 1, startTime + 5)
+   self.notes:createNote("E3", 1, startTime + 3)
+   self.notes:createNote("E3", 1, startTime + 5)
 
-   notes:createNote("A2", 1, startTime + 6)
+   self.notes:createNote("A2", 1, startTime + 6)
 end
 
 function MainGame:measure5(startTime)
-   notes:createNote("A2", 1, startTime)
+   self.notes:createNote("A2", 1, startTime)
 
-   notes:createNote("C3", 1, startTime + 1)
-   notes:createNote("C3", 1, startTime + 3)
+   self.notes:createNote("C3", 1, startTime + 1)
+   self.notes:createNote("C3", 1, startTime + 3)
 
-   notes:createNote("F2", 1, startTime + 4)
-   notes:createNote("F2", 1, startTime + 6)
+   self.notes:createNote("F2", 1, startTime + 4)
+   self.notes:createNote("F2", 1, startTime + 6)
 
-   notes:createNote("A2", 1, startTime + 7)
+   self.notes:createNote("A2", 1, startTime + 7)
 end
 
 function MainGame:measure6(startTime)
-   notes:createNote("A2", 1, startTime)
+   self.notes:createNote("A2", 1, startTime)
 
-   notes:createNote("G2", 1, startTime + 1)
-   notes:createNote("G2", 1, startTime + 3)
+   self.notes:createNote("G2", 1, startTime + 1)
+   self.notes:createNote("G2", 1, startTime + 3)
 
-   notes:createNote("B2", 1, startTime + 4)
-   notes:createNote("B2", 1, startTime + 6)
+   self.notes:createNote("B2", 1, startTime + 4)
+   self.notes:createNote("B2", 1, startTime + 6)
 end
 
 function MainGame:measure7(startTime)
-   notes:createNote("C2", 1, 49)
-   notes:createNote("C4", 2, 49)
+   self.notes:createNote("C2", 1, 49)
+   self.notes:createNote("C4", 2, startTime)
 
-   notes:createNote("C2", 1, 51)
+   self.notes:createNote("C2", 1, startTime + 2)
 
-   notes:createNote("C3", 1, 52)
-   notes:createNote("E3", 1, 52)
-   notes:createNote("C4", 2, 52)
+   self.notes:createNote("C3", 1, startTime + 3)
+   self.notes:createNote("E3", 1, startTime + 3)
+   self.notes:createNote("C4", 2, startTime + 3)
 
-   notes:createNote("C3", 1, 54)
-   notes:createNote("E3", 1, 54)
+   self.notes:createNote("C3", 1, startTime + 5)
+   self.notes:createNote("E3", 1, startTime + 5)
 
-   notes:createNote("A1", 1, 55)
-   notes:createNote("C4", 1, 55)
+   self.notes:createNote("A1", 1, startTime + 6)
+   self.notes:createNote("C4", 1, startTime + 6)
 end
 
 function MainGame:measure8(startTime)
-   notes:createNote("A1", 1, 57)
+   self.notes:createNote("A1", 1, startTime)
 
-   notes:createNote("A2", 1, startTime + 1)
-   notes:createNote("C3", 1, startTime + 1)
+   self.notes:createNote("A2", 1, startTime + 1)
+   self.notes:createNote("C3", 1, startTime + 1)
 
-   notes:createNote("A2", 1, startTime + 3)
-   notes:createNote("C3", 1, startTime + 3)
+   self.notes:createNote("A2", 1, startTime + 3)
+   self.notes:createNote("C3", 1, startTime + 3)
 
-   notes:createNote("D2", 1, startTime + 4)
+   self.notes:createNote("D2", 1, startTime + 4)
 
-   notes:createNote("D2", 1, startTime + 6)
-   notes:createNote("C4", 1, startTime + 6)
+   self.notes:createNote("D2", 1, startTime + 6)
+   self.notes:createNote("C4", 1, startTime + 6)
 
-   notes:createNote("D3", 1, startTime + 7)
-   notes:createNote("F3", 1, startTime + 7)
-   notes:createNote("B3", 1, startTime + 7)
+   self.notes:createNote("D3", 1, startTime + 7)
+   self.notes:createNote("F3", 1, startTime + 7)
+   self.notes:createNote("B3", 1, startTime + 7)
 end
 
 function MainGame:measure9(startTime)
-   notes:createNote("D3", 1, startTime)
-   notes:createNote("F3", 1, startTime)
-   notes:createNote("A3", 1, startTime)
+   self.notes:createNote("D3", 1, startTime)
+   self.notes:createNote("F3", 1, startTime)
+   self.notes:createNote("A3", 1, startTime)
 
-   notes:createNote("G1", 1, startTime + 1)
-   notes:createNote("B3", 2, startTime + 1)
+   self.notes:createNote("G1", 1, startTime + 1)
+   self.notes:createNote("B3", 2, startTime + 1)
 
-   notes:createNote("G1", 1, startTime + 3)
-   notes:createNote("C4", 1, startTime + 3)
+   self.notes:createNote("G1", 1, startTime + 3)
+   self.notes:createNote("C4", 1, startTime + 3)
 
-   notes:createNote("G2", 1, startTime + 4)
-   notes:createNote("B2", 1, startTime + 4)
-   notes:createNote("D4", 1, startTime + 4)
+   self.notes:createNote("G2", 1, startTime + 4)
+   self.notes:createNote("B2", 1, startTime + 4)
+   self.notes:createNote("D4", 1, startTime + 4)
 
-   notes:createNote("G2", 1, startTime + 6)
-   notes:createNote("B2", 1, startTime + 6)
+   self.notes:createNote("G2", 1, startTime + 6)
+   self.notes:createNote("B2", 1, startTime + 6)
 end
 
 function MainGame:measure10(startTime)
-   notes:createNote("C2", 1, startTime)
-   notes:createNote("E4", 2, startTime)
+   self.notes:createNote("C2", 1, startTime)
+   self.notes:createNote("E4", 2, startTime)
 
-   notes:createNote("C2", 1, startTime + 2)
+   self.notes:createNote("C2", 1, startTime + 2)
 
-   notes:createNote("C3", 1, startTime + 3)
-   notes:createNote("E3", 1, startTime + 3)
-   notes:createNote("E4", 2, startTime + 3)
+   self.notes:createNote("C3", 1, startTime + 3)
+   self.notes:createNote("E3", 1, startTime + 3)
+   self.notes:createNote("E4", 2, startTime + 3)
 
-   notes:createNote("C3", 1, startTime + 5)
-   notes:createNote("E3", 1, startTime + 5)
+   self.notes:createNote("C3", 1, startTime + 5)
+   self.notes:createNote("E3", 1, startTime + 5)
 
-   notes:createNote("A1", 1, startTime + 6)
-   notes:createNote("E4", 2, startTime + 6)
+   self.notes:createNote("A1", 1, startTime + 6)
+   self.notes:createNote("E4", 2, startTime + 6)
 end
 
 function MainGame:measure11(startTime)
-   notes:createNote("A1", 1, startTime)
+   self.notes:createNote("A1", 1, startTime)
 
-   notes:createNote("A2", 1, startTime + 1)
-   notes:createNote("C3", 1, startTime + 1)
+   self.notes:createNote("A2", 1, startTime + 1)
+   self.notes:createNote("C3", 1, startTime + 1)
 
-   notes:createNote("A2", 1, startTime + 3)
-   notes:createNote("C3", 1, startTime + 3)
+   self.notes:createNote("A2", 1, startTime + 3)
+   self.notes:createNote("C3", 1, startTime + 3)
 
-   notes:createNote("F1", 1, startTime + 4)
+   self.notes:createNote("F1", 1, startTime + 4)
 
-   notes:createNote("F1", 1, startTime + 6)
-   notes:createNote("E4", 1, startTime + 6)
+   self.notes:createNote("F1", 1, startTime + 6)
+   self.notes:createNote("E4", 1, startTime + 6)
 
-   notes:createNote("F2", 1, startTime + 7)
-   notes:createNote("A2", 1, startTime + 7)
-   notes:createNote("D4", 1, startTime + 7)
+   self.notes:createNote("F2", 1, startTime + 7)
+   self.notes:createNote("A2", 1, startTime + 7)
+   self.notes:createNote("D4", 1, startTime + 7)
 
 end
 
